b61cd308e2b545c0306b07ecb69d81880406371a
Max Peterson <max@cochin.local>
2010-09-28 15:00:19 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = "djangogcal",
+    packages = ["djangogcal", ],
+    version = "0.1",
+    description = "A Django application allowing developers to synchronise instances of their models with Google Calendar.",
+    author = "Incuna Ltd",
+    author_email = "admin@incuna.com",
+    url = "http://incuna.com/",
+)
