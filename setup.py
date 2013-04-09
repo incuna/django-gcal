40510ae903dfcf062f9f5e01a2219c74f26a6266
Marc Tamlyn <marc.tamlyn@gmail.com>
2013-04-09 10:04:25 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = "djangogcal",
     packages = ["djangogcal", ],
-    version = "0.1",
+    version = "0.2",
     description = "A Django application allowing developers to synchronise instances of their models with Google Calendar.",
     author = "Incuna Ltd",
     author_email = "admin@incuna.com",
